1fec071acc3c91e0840c7981f31b5285aa429a80
Mario Falchi et al <mfalchi@cern.ch>
2017-02-02 13:02:02 +0100

    use pkill instead of killall.

--- script/BAM2fastq/KILL.sh
+++ script/BAM2fastq/KILL.sh
@@ -2,11 +2,11 @@
 
 #set -x
 
-echo "killall -w -s 9 /oplashare/data/mfalchi/GeneROOT/script/BAM2fastq/run_bamToFastq.sh"
-killall -w -s 9 /oplashare/data/mfalchi/GeneROOT/script/BAM2fastq/run_bamToFastq.sh
-echo "killall -w -s 9 /oplashare/data/mfalchi/parallel-20161122/src/parallel"
-killall -w -s 9 /oplashare/data/mfalchi/parallel-20161122/src/parallel
-echo "killall -w -s 9 /oplashare/data/mfalchi/sambamba_v0.6.5"
-killall -w -s 9 /oplashare/data/mfalchi/sambamba_v0.6.5
+echo "pkill -9 run_bamToFastq.sh"
+pkill -9 run_bamToFastq.sh
+echo "pkill -9 -f \"perl /oplashare/data/mfalchi/parallel\""
+pkill -9 -f "perl /oplashare/data/mfalchi/parallel"
+echo "pkill -9 sambamba_v0.6.5"
+pkill -9 sambamba_v0.6.5
 
 exit 0
